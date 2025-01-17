b6f13219b3cab0f6cf37ec191ab20526050ff4bc
JhayTech <jrlabrador21@gmail.com>
2025-01-17 15:27:20 +0800

    My Repo

--- InventoryMgmtQA/Model/ProductTest.cs
+++ InventoryMgmtQA/Model/ProductTest.cs
@@ -14,9 +14,9 @@ namespace InventoryMgmtQA.Model
             // create a new product with compliant attribute values
             Product product = new()
             {
-                Name = "TestProduct",
-                QuantityInStock = 1,
-                Price = 1.0M
+                Name = "Apple",
+                QuantityInStock = 6,
+                Price = 125
             };
 
             var results = new List<ValidationResult>();
@@ -26,14 +26,33 @@ namespace InventoryMgmtQA.Model
             // the product must be valid since all attributes values validated correctly
             Assert.IsTrue(isProductValid);
         }
+
+        [TestMethod]
+        public void TestAddProductQuantityNegative()
+        {
+            Product product = new()
+            {
+                Name = "Pears",
+                QuantityInStock = -5,// test for negative quantiy
+                Price = 100
+            };
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+            bool isProductValid = Validator.TryValidateObject(product, context, results, true);
+
+            // the product must NOT be valid since the Quantity attribute has invalid value
+            Assert.IsFalse(isProductValid);
+        }
+
         [TestMethod]
         public void TestAddProductPriceNegative()
         {
             Product product = new()
             {
-                Name = "TestProduct",
-                QuantityInStock = 1,
-                Price = -1.0M // test for negative price
+                Name = "Pears",
+                QuantityInStock = 5,
+                Price = -100 // test for negative price
             };
 
             var results = new List<ValidationResult>();
@@ -44,6 +63,84 @@ namespace InventoryMgmtQA.Model
             Assert.IsFalse(isProductValid);
         }
 
-        // add more test cases here
+        [TestMethod]
+        public void TestAddProductPricenQtyNegative()
+        {
+            Product product = new() // test for negative price n qty
+            {
+                Name = "Apple",
+                QuantityInStock = -5,
+                Price = -100 
+            };
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+            bool isProductValid = Validator.TryValidateObject(product, context, results, true);
+
+            // the product must NOT be valid since the Price and Qty attribute has invalid value
+            Assert.IsFalse(isProductValid);
+        }
+
+        [TestMethod]
+        public void TestAddProductQtyInvalid()
+        {
+            Product product = new() // test for invalid qty input
+            {
+                Name = "Apple",
+                QuantityInStock = -5,
+                Price = 0
+            };
+
+            bool isProductValid = false;
+
+            try
+            {
+                // Simulate invalid price input (convert text to double)
+                product.Price = (decimal)Convert.ToDouble("5m");
+            }
+            catch (FormatException ex)
+            {
+                // If FormatException occurs, test invalid input
+                isProductValid = false;
+                Console.WriteLine($"Validation Error: {ex.Message}");
+            }
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+
+            // the product must NOT be valid since the Qty attribute has invalid value
+            Assert.IsFalse(isProductValid);
+        }
+
+        [TestMethod]
+        public void TestAddProductPriceInvalid()
+        {
+            Product product = new() // test for invalid price input
+            {
+                Name = "Apple",
+                QuantityInStock = 5,
+                Price = 0
+            };
+
+            bool isProductValid = false;
+
+            try
+            {
+                // Simulate invalid price input (convert text to double)
+                product.Price = (decimal)Convert.ToDouble("100m");
+            }
+            catch (FormatException ex)
+            {
+                // If FormatException occurs, test invalid input
+                isProductValid = false;
+                Console.WriteLine($"Validation Error: {ex.Message}");
+            }
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+
+            // the product must NOT be valid since the Price attribute has invalid value
+            Assert.IsFalse(isProductValid);
+        }
     }
 }
